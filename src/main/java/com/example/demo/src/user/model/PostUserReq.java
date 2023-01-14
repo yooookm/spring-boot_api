ff567b62333b5a0adfda534139ca4a2ead8ff179
Yoo KyeongMi <89245114+YooKyungmi@users.noreply.github.com>
2023-01-14 16:40:09 +0900

    sample

---
+++ src/main/java/com/example/demo/src/user/model/PostUserReq.java
@@ -0,0 +1,17 @@
+package com.example.demo.src.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostUserReq {
+    private String UserName;
+    private String id;
+    private String email;
+    private String password;
+}
